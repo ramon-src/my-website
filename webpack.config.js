87ca58b62b65ab60009d3f615dd06282ccc72f94
rschmidt <ramonroc@gmail.com>
2018-12-22 17:11:46 -0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+var path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["@babel/preset-env"] }
+                }
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "stylus-loader" // compiles Stylus to CSS
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
